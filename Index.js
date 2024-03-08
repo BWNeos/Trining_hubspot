4e1979d213d5e9649ada952e5f28300435c83279
Bharat Wakte <bharat.wakte@neosalpha.com>
2024-03-08 15:48:18 +0530

    Commit it 1

--- Index.js
+++ Index.js
@@ -1,3 +1,14 @@
 const age =24;
-const name = 'bharat';
+const name1 = 'bharat';
 
+function isAbove18(){
+    return age>=18
+}
+
+function isHappy(){
+    return true
+} 
+
+function print(){
+    console.log('My name is ${name1}')
+}
---
+++ Test.js
@@ -0,0 +1,6 @@
+const a = 1;
+const b = 2;
+
+function add(){
+    return 1+2
+}
